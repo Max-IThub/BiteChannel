6add31013bda4dd9437201ae2951dcdaabe25e40
Max <Nikolaev.max05@gmail.com>
2023-02-10 00:15:24 +0300

    new lessons

---
+++ script.js
@@ -0,0 +1,10 @@
+//Всего в JavaScript существует 8 типов данных
+//Number - 12, 11, 0, -32, 1000000
+//String - "", '', ``
+//Boolean - true, false
+
+// console.log('String',"- 55+5 тип данных String")
+// console.log()
+// console.log(11+5,23,5.5,-45, 'это тип данных Number')
+// console.log()
+// console.log('true', false, false, 'тип данных Boolean')
